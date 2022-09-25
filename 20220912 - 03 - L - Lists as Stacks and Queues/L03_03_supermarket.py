f32b0b5f13ca3fe1b7540892adda3b7ecd4fcfc9
theterminal <kirilk777@gmail.com>
2022-09-26 00:39:13 +0300

    Initial

---
+++ 20220912 - 03 - L - Lists as Stacks and Queues/L03_03_supermarket.py
@@ -0,0 +1,40 @@
+# 20220912 - Python - Python Advanced - Lists as Stacks and Queues
+# Supermarket - judge url: https://judge.softuni.org/Contests/Practice/Index/1830#2
+
+from collections import deque
+
+
+# _____________________ version 2 ____________________ judge 100%
+
+
+q = deque()
+
+while True:
+    name = input()
+    if name == 'End':
+        print(f'{len(q)} people remaining.')
+        break
+
+    if name == 'Paid':
+        print('\n'.join(q))
+        q.clear()
+        continue
+    q.append(name)
+
+
+# _____________________ version 1 ____________________ judge 100%
+
+
+q = deque()
+
+while True:
+    name = input()
+    if name == 'End':
+        print(f'{len(q)} people remaining.')
+        break
+
+    if name == 'Paid':
+        while len(q) > 0:
+            print(q.popleft())
+        continue
+    q.append(name)
