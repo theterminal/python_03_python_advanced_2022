8efa185c8c3c80f692a10a5f71b6245008276166
theterminal <kirilk777@gmail.com>
2022-09-27 00:58:32 +0300

    Initial

---
+++ 20220912 - 03 - L - Lists as Stacks and Queues/L03_notes_01_stack.py
@@ -0,0 +1,29 @@
+# 20220912 - Python - Python Advanced - Lists as Stacks and Queues
+
+
+stack = [3, 4, 5]
+print(stack)
+
+stack.append(6)
+stack.append(7)
+print(stack)
+
+stack.pop()
+print(stack)
+
+stack.pop()
+stack.pop()
+print(stack)
+
+
+# ----------------------------
+
+
+stack = []
+
+print(stack)
+print(len(stack))
+print(stack[-1])
+
+
+# ----------------------------
