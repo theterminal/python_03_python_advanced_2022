efd347414b2e78e77e9fbadea86203328191fe59
theterminal <kirilk777@gmail.com>
2022-09-28 08:02:05 +0300

    Initial

---
+++ 20220913 - 04 - E - Lists as Stacks and Queues/L04_01_reverse_numbers.py
@@ -0,0 +1,22 @@
+# 20220913 - Python - Python Advanced - Lists as Stacks and Queues
+# 01 - Reverse Strings - judge url: https://judge.softuni.org/Contests/Practice/Index/1830#0
+
+
+# ___________ version 1 ____________ judge 100%
+
+
+stack = input().split()
+
+result = []
+for _ in range(len(stack)):
+    result.append(stack.pop())
+print(' '.join(result))
+
+
+# ___________ version 2 ____________ judge 100%
+
+
+stack = input().split()
+
+for _ in range(len(stack)):
+    print(stack.pop(), end=' ')
