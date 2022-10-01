997fe830657a522a7184714d41d0c8d8fd6730e0
theterminal <kirilk777@gmail.com>
2022-10-01 11:10:56 +0300

    Initial

---
+++ 20220915 - 05 - L - Tuples and Sets/Methods/L05_tuple_02_count()_index().py
@@ -0,0 +1,21 @@
+# 20220915 - Python - Python Advanced - Tuples and Sets
+
+
+# ____________ count() _______________ it counts the occurrences of the specified element
+
+
+t = (10, 10, 23, 34, 45, 23, 10, 45)
+print(t.count(10))                          # 3
+print(t.count(45))                          # 2
+
+
+# ____________ index() _______________ returns the index of the specified element
+
+
+print(t.index(45))
+
+
+# ____________ len() _________________ used to count the number of the elements in the tuple
+
+
+print(len(t))
