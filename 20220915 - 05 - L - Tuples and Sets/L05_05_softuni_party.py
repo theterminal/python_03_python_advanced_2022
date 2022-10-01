73a1da57de0413da8475c082de362ca2599080e8
theterminal <kirilk777@gmail.com>
2022-10-02 01:50:51 +0300

    Initial

---
+++ 20220915 - 05 - L - Tuples and Sets/L05_05_softuni_party.py
@@ -0,0 +1,34 @@
+# 20220915 - Python - Python Advanced - Tuples and Sets
+# 05 - SoftUni Party - judge url: https://judge.softuni.org/Contests/Practice/Index/1832#4
+
+
+# _______________ version 1 _________________ judge 100%
+
+
+num_guests = int(input())
+reservations_set_vip = set()
+reservations_set_reg = set()
+
+for i in range(num_guests):
+    reservation_in = input()
+    if reservation_in[0].isdigit():
+        reservations_set_vip.add(reservation_in)
+    else:
+        reservations_set_reg.add(reservation_in)
+
+while True:
+    guest_in = input()
+    if guest_in == 'END':
+        break
+
+    if guest_in[0].isdigit():
+        reservations_set_vip.discard(guest_in)
+    else:
+        reservations_set_reg.discard(guest_in)
+
+print(len(reservations_set_vip) + len(reservations_set_reg))
+
+if reservations_set_vip:
+    print('\n'.join(sorted(list(reservations_set_vip))))
+if reservations_set_reg:
+    print('\n'.join(sorted(list(reservations_set_reg))))
