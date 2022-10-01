cae6c14c8c9d0cebf5ba55ac496191ac8e781875
theterminal <kirilk777@gmail.com>
2022-10-02 01:49:01 +0300

    Initial

---
+++ 20220915 - 05 - L - Tuples and Sets/Methods/L05_tuple_03_unpacking.py
@@ -0,0 +1,22 @@
+# 20220915 - Python - Python Advanced - Tuples and Sets
+
+
+t = (1, 2, 3)
+print(t)
+
+
+# __________ unpacking ____________
+
+
+x, y, z = t                 # assigns 3 nuw variables without destroying the tuple
+print(x)
+print(y)
+print(z)
+
+
+print(t)                    # 't' still exists
+
+x1, y1, z1 = t
+print(x1)
+print(y1)
+print(z1)
