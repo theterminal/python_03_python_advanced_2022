71e89181b90417e709db0630388cf9244978fedb
theterminal <kirilk777@gmail.com>
2022-10-02 01:50:38 +0300

    Initial

---
+++ 20220915 - 05 - L - Tuples and Sets/L05_03_unique_names.py
@@ -0,0 +1,30 @@
+# 20220915 - Python - Python Advanced - Tuples and Sets
+# 03 - Unique Names - judge url: https://judge.softuni.org/Contests/Practice/Index/1832#2
+
+
+# _______________ version 1 _________________ judge 100%
+
+
+number_names = int(input())
+unique_names = set()
+
+for i in range(number_names):
+    unique_names.add(input())
+
+for unique_name in unique_names:
+    print(unique_name)
+
+
+# _______________ version 2 _________________ judge 100%
+
+
+number_names = int(input())
+names = []
+
+for i in range(number_names):
+    names.append(input())
+
+unique_names = set(name for name in names)
+
+for name in names:
+    print(name)
