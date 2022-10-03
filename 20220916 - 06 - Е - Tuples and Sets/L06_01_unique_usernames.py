0953534a914573b950958c9daf537b4be24b7a21
theterminal <kirilk777@gmail.com>
2022-10-03 17:13:56 +0300

    Initial

---
+++ 20220916 - 06 - Е - Tuples and Sets/L06_01_unique_usernames.py
@@ -0,0 +1,15 @@
+# 20220916 - Python - Python Advanced - Tuples and Sets
+# 01 - Unique Usernames - judge url: https://judge.softuni.org/Contests/Compete/Index/1833#0
+
+
+# _______________ version 1 _________________ judge 100%
+
+
+number_of_usernames = int(input())
+unique_usernames = set()
+
+for _ in range(number_of_usernames):
+    unique_usernames.add(input())
+
+if unique_usernames:
+    print('\n'.join(unique_usernames))
