fb5b24f05a70b4b6c016983114f7ff4ecc282209
theterminal <kirilk777@gmail.com>
2022-10-03 17:14:31 +0300

    Initial

---
+++ 20220916 - 06 - Е - Tuples and Sets/L06_03_periodic_table.py
@@ -0,0 +1,17 @@
+# 20220916 - Python - Python Advanced - Tuples and Sets
+# 03 - Periodic Table - judge url: https://judge.softuni.org/Contests/Compete/Index/1833#2
+
+
+# _______________ version 1 _________________ judge 100%
+
+
+lines_in = int(input())
+unique_elements = set()
+
+for _ in range(lines_in):
+    data = input().split()
+    for element in data:
+        unique_elements.add(element)
+
+if unique_elements:
+    print('\n'.join(unique_elements))
