ac8102f4bf6510c35ba5aa9efd4a6e792df886f5
theterminal <kirilk777@gmail.com>
2022-10-08 07:55:52 +0300

    Initial

---
+++ 20220921 - 09 - E - Multidimensional Lists - 1/L09_02_diagonal_difference.py
@@ -0,0 +1,23 @@
+# 20220921 - Python - Python Advanced - Multidimensional Lists
+# 02 - Diagonal Difference - judge url: https://judge.softuni.org/Contests/Compete/Index/1835#1
+
+
+# _______________ version 1 _________________ judge 100%
+
+
+matrix = [[int(x) for x in input().split()] for y in range(int(input()))]
+# matrix = [list(map(int, input().split())) for y in range(int(input()))]                 # done using 'map'
+
+primary_diagonal = []
+secondary_diagonal = []
+
+sum_primary_diagonal = 0
+sum_secondary_diagonal = 0
+
+last_index = len(matrix) - 1
+
+for i in range(len(matrix)):
+    primary_diagonal.append(matrix[i][i])
+    secondary_diagonal.append(matrix[i][last_index - i])
+
+print(abs(sum(primary_diagonal) - sum(secondary_diagonal)))
