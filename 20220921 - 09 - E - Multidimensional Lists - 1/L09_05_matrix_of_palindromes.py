1d6436942e1efd43b8c6cd72325fd136cff32914
theterminal <kirilk777@gmail.com>
2022-10-08 07:56:13 +0300

    Initial

---
+++ 20220921 - 09 - E - Multidimensional Lists - 1/L09_05_matrix_of_palindromes.py
@@ -0,0 +1,18 @@
+# 20220921 - Python - Python Advanced - Multidimensional Lists
+# 05 - Matrix of Palindromes - judge url: https://judge.softuni.org/Contests/Compete/Index/1835#4
+
+
+# _______________ version 1 _________________ judge 100%
+
+
+rows, columns = list(map(int, input().split()))
+ascii_a = 97
+matrix = []
+
+for row in range(rows):
+    sub_matrix = []
+    for col in range(columns):
+        result = chr(ascii_a + row) + chr(ascii_a + col + row) + chr(ascii_a + row)
+        sub_matrix.append(result)
+
+    print(*sub_matrix)
