b2dff289baff136ad557cadcbcef2c058edeed40
theterminal <kirilk777@gmail.com>
2022-10-08 07:55:36 +0300

    Initial

---
+++ 20220921 - 09 - E - Multidimensional Lists - 1/L09_01_diagonals.py
@@ -0,0 +1,24 @@
+# 20220921 - Python - Python Advanced - Multidimensional Lists
+# 01 - Diagonals - judge url: https://judge.softuni.org/Contests/Compete/Index/1835#0
+
+
+# _______________ version 1 _________________ judge 100%
+
+
+matrix = [[int(x) for x in input().split(',')] for y in range(int(input()))]
+# matrix = [list(map(int, input().split(', '))) for y in range(int(input()))]                 # done using 'map'
+
+primary_diagonal = []
+secondary_diagonal = []
+
+sum_primary_diagonal = 0
+sum_secondary_diagonal = 0
+
+last_index = len(matrix) - 1
+
+for i in range(len(matrix)):
+    primary_diagonal.append(matrix[i][i])
+    secondary_diagonal.append(matrix[i][last_index - i])
+
+print(f'Primary diagonal: {", ".join(list(map(str, primary_diagonal)))}. Sum: {sum(primary_diagonal)}')
+print(f'Secondary diagonal: {", ".join(list(map(str, secondary_diagonal)))}. Sum: {sum(secondary_diagonal)}')
