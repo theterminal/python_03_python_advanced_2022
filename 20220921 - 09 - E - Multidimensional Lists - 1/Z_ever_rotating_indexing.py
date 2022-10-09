36da57833e5149b684a06de5849888764c29e3e9
theterminal <kirilk777@gmail.com>
2022-10-09 10:33:54 +0300

    Initial

---
+++ 20220921 - 09 - E - Multidimensional Lists - 1/Z_ever_rotating_indexing.py
@@ -0,0 +1,21 @@
+# 20220921 - Python - Python Advanced - Multidimensional Lists
+
+# Ever rotating indexing...
+
+# using '%' for incrementing index depending on the needed length of the string
+
+
+rows = 100
+columns = 153
+string = 'There is a war on for your mind!'
+index = 0
+
+for row in range(rows):
+    result = ''
+    for col in range(columns):
+        result += string[index % len(string)]
+        # print(index % len(string), end='')
+        index += 1
+    # print()
+    print(result)
+    # print()
