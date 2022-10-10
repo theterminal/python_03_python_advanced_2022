21db50e93f91f3cf6d0b82c6b27d897d0de05b38
theterminal <kirilk777@gmail.com>
2022-10-11 00:18:39 +0300

    Initial

---
+++ 20220926 - 11 - L - Functions - Advanced/L11_01_multiplication_function.py
@@ -0,0 +1,18 @@
+# 20220926 - Python - Python Advanced - Functions Advanced
+# 01 - Multiplication Function - judge url: https://judge.softuni.org/Contests/Practice/Index/1838#0
+
+
+# _______________ version 1 _________________ judge 100%
+
+
+def multiply(*args):
+    result = 1
+    for element in args:
+        result *= element
+
+    return result
+
+
+print(multiply(1, 4, 5))
+print(multiply(4, 5, 6, 1, 3))
+print(multiply(2, 0, 1000, 5000))
