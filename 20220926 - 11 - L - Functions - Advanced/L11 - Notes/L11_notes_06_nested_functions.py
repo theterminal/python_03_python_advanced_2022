5bd18d96f1ab82bdc4114115f46c8cdb1f587982
theterminal <kirilk777@gmail.com>
2022-10-11 00:16:35 +0300

    Initial

---
+++ 20220926 - 11 - L - Functions - Advanced/L11 - Notes/L11_notes_06_nested_functions.py
@@ -0,0 +1,21 @@
+# 20220926 - Python - Python Advanced - Functions Advanced
+
+# Nested Functions
+
+
+def func_main():
+    def nested_func_1():
+        return f'Hello from nested 1'
+
+    def nested_func_2():
+        return f'Hello from nested 2'
+
+    def nested_func_3():
+        return f'Hello from nested 3'
+
+    result = nested_func_1() + '\n' + nested_func_2() + '\n' + nested_func_3()
+
+    return result
+
+
+print(func_main())
