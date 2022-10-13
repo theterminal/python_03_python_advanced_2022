434028cda2a30313bb1bfb6dd28c0288abe943a7
theterminal <kirilk777@gmail.com>
2022-10-13 06:58:19 +0300

    Initial

---
+++ 20220926 - 11 - L - Functions - Advanced/L11_06_recursive_power.py
@@ -0,0 +1,14 @@
+# 20220926 - Python - Python Advanced - Functions Advanced
+# 06 - Recursive Power - judge url: https://judge.softuni.org/Contests/Practice/Index/1838#5
+
+
+# _______________ version 1 _________________ judge 100%
+
+
+def recursive_power(number, power):
+    if power == 0:
+        return 1
+    return number * recursive_power(number, power - 1)
+
+
+print(recursive_power(2, 10))
