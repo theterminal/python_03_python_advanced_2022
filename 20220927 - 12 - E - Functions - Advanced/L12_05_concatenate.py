35787c971a5471fbd38ff4a39da2fcf4e82bb999
theterminal <kirilk777@gmail.com>
2022-10-14 06:15:24 +0300

    Initial

---
+++ 20220927 - 12 - E - Functions - Advanced/L12_05_concatenate.py
@@ -0,0 +1,19 @@
+# 20220927 - Python - Python Advanced - Functions Advanced
+# 05 - Concatenate - judge url: https://judge.softuni.org/Contests/Compete/Index/1839#4
+
+
+# _______________ version 1 _________________ judge 100%
+
+
+def concatenate(*args, **kwargs):
+    string = ''.join(args)
+
+    for k, v in kwargs.items():
+        if k in string:                                   # could do it without this line
+            string = string.replace(k, v)
+
+    return string
+
+
+print(concatenate("I", " ", "Love", " ", "Cythons", C="P", s="", java='Java'))
+
