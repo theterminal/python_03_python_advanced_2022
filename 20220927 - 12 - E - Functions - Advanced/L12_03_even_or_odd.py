d5a3ad543775d2a1a2449184e1ea521b951e02a7
theterminal <kirilk777@gmail.com>
2022-10-14 06:15:12 +0300

    Initial

---
+++ 20220927 - 12 - E - Functions - Advanced/L12_03_even_or_odd.py
@@ -0,0 +1,20 @@
+# 20220927 - Python - Python Advanced - Functions Advanced
+# 03 - Even or Odd - judge url: https://judge.softuni.org/Contests/Compete/Index/1839#2
+
+
+# _______________ version 1 _________________ judge 100%
+
+
+def even_odd(*args):
+    args, command = args[:-1],args[-1]
+
+    if command == 'even':
+        result = [x for x in args if x % 2 == 0]
+    else:
+        result = [x for x in args if x % 2 == 1]
+
+    return result
+
+
+print(even_odd(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "odd"))
+
