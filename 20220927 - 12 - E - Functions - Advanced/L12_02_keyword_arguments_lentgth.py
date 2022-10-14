f576ea76d02f65afda31f1ea04469bcc8b5552ff
theterminal <kirilk777@gmail.com>
2022-10-14 06:15:06 +0300

    Initial

---
+++ 20220927 - 12 - E - Functions - Advanced/L12_02_keyword_arguments_lentgth.py
@@ -0,0 +1,14 @@
+# 20220927 - Python - Python Advanced - Functions Advanced
+# 02 - Keyword Arguments Lent=gth - judge url: https://judge.softuni.org/Contests/Compete/Index/1839#1
+
+
+# _______________ version 1 _________________ judge 100%
+
+
+def kwargs_length(**dict):
+    return len(dict.keys())
+
+
+dictionary = {'name': 'Peter', 'age': 25}
+
+print(kwargs_length(**dictionary))
