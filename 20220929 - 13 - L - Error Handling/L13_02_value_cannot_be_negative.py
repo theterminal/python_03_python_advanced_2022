affa90a03480a9c915b99d267277d8fe5b0495c6
theterminal <kirilk777@gmail.com>
2022-10-15 07:14:20 +0300

    Initial

---
+++ 20220929 - 13 - L - Error Handling/L13_02_value_cannot_be_negative.py
@@ -0,0 +1,15 @@
+# 20220929 - Python - Python Advanced - Error Handling
+# 02 - Value Cannot Be Negative - judge url: No judge for this problem
+
+
+# _______________ version 1 _________________
+
+
+class ValueCannotBeNegative(Exception):
+    """Value cannot be below zero!"""
+
+
+for i in range(5):
+    number = int(input())
+    if number < 0:
+        raise ValueCannotBeNegative
