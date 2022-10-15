a4210f2c2b659f93f3122c1d2c6b0bcc2d53df20
theterminal <kirilk777@gmail.com>
2022-10-15 07:12:17 +0300

    Initial

---
+++ 20220927 - 12 - E - Functions - Advanced/L12_09_recursion_palindrome.py
@@ -0,0 +1,19 @@
+# 20220927 - Python - Python Advanced - Functions Advanced
+# 09 - Recursion Palindrome - judge url: https://judge.softuni.org/Contests/Compete/Index/1839#8
+
+
+# _______________ version 1 _________________ judge 100%
+
+
+def palindrome(word: str, index: int):
+    if index == len(word) // 2:
+        return f'{word} is a palindrome'
+
+    first, last = word[index], word[-1 - index]
+    if first != last:
+        return f'{word} is not a palindrome'
+
+    return palindrome(word, index + 1)
+
+
+print(palindrome("abcba", 0))
