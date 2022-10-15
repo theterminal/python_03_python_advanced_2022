e152edcdcf0caffb7785f206f17adeba92d13cc2
theterminal <kirilk777@gmail.com>
2022-10-15 07:14:02 +0300

    Initial

---
+++ 20220929 - 13 - L - Error Handling/L13_01_so_many_exceptiona.py
@@ -0,0 +1,23 @@
+# 20220929 - Python - Python Advanced - Error Handling
+# 01 - So Many Exceptions - judge url: No judge for this problem
+
+
+# _______________ version 1 _________________
+
+
+for i in range(4):
+    numbers_list = [int(x) for x in input().split(", ")]
+    result = 1
+
+    for number in numbers_list:
+
+        if number <= 5:
+            result *= number
+
+        elif number <= 10:
+            result /= number
+
+    if result >= 1:
+        print(f'{result:.0f}')
+    else:
+        print(result)
