5a32998cedc92130beb2f037ab51b858cdab5c43
theterminal <kirilk777@gmail.com>
2022-10-16 08:15:19 +0300

    Initial

---
+++ 20220930 - 14 - E - Error Handling/L14_01_numbers_dictionary.py
@@ -0,0 +1,44 @@
+# 20220930 - Python - Python Advanced - Error Handling
+# 01 - Numbers Dictionary - judge url: No judge for this problem
+
+
+# _______________ version 1 _________________
+
+
+numbers_dictionary = {}
+
+line = input()
+while line != "Search":
+    number_as_string = line
+
+    try:
+        number = int(input())
+        numbers_dictionary[number_as_string] = number
+    except ValueError:
+        print('The variable number must be an integer')
+
+    line = input()
+
+line = input()
+while line != "Remove":
+    searched = line
+
+    try:
+        print(numbers_dictionary[searched])
+    except KeyError:
+        print('Number does not exist in dictionary')
+
+    line = input()
+
+line = input()
+while line != "End":
+    searched = line
+
+    try:
+        del numbers_dictionary[searched]
+    except KeyError:
+        print('Number does not exist in dictionary')
+
+    line = input()
+
+print(numbers_dictionary)
