d2bcf7908a86ae89aa153214b0988eeeee9ac51f
theterminal <kirilk777@gmail.com>
2022-10-18 11:54:15 +0300

    Initial

---
+++ 20221003 - 15 - L - File Handling/L15_04_file_delete.py
@@ -0,0 +1,14 @@
+# 20221003 - Python - Python Advanced - File Handling
+# 04 - File Delete
+
+# _______________ version 1 _________________
+
+
+from os import remove
+
+
+try:
+    file_path = './my_first_file.txt'
+    remove(file_path)
+except FileNotFoundError:
+    print('File already deleted!')
