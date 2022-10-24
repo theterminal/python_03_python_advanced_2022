6acd4f46aaa4b7d6f132836313e7c2347af88c5a
theterminal <kirilk777@gmail.com>
2022-10-24 07:53:34 +0300

    Initial

---
+++ 20221010 - 19 - L - Modules/L19_03_triangle.py
@@ -0,0 +1,17 @@
+# 20221010 - Python - Python Advanced - Modules
+# 03 - Triangle
+
+
+# ----------------- version 1 -------------------
+
+
+from utils_L19_03.print_figures import print_triangle, print_square, print_rectangle
+from utils_L19_03.print_helper import print_line
+
+print_triangle(6)
+print()
+
+print_square(3)
+print()
+
+print_rectangle(3, 4)
