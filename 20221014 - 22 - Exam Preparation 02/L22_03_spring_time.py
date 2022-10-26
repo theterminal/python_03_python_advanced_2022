c9a56de29efb826a295d3a820eaf0b7d6c8ca3bb
theterminal <kirilk777@gmail.com>
2022-10-27 00:06:08 +0300

    Initial

---
+++ 20221014 - 22 - Exam Preparation 02/L22_03_spring_time.py
@@ -0,0 +1,37 @@
+# 20221014 - Python - Python Advanced - Exam Preparation 02
+# 03 - Springtime - judge: https://judge.softuni.org/Contests/Practice/Index/3374#2
+
+
+# _______________ version 1 _________________ judge 100%
+
+
+def start_spring(**data):
+    output = {}
+    result = []
+
+    for k, v in data.items():
+        if v not in output.keys():
+            output[v] = []
+        output[v].append(k)
+
+    output = sorted(output.items(), key=lambda x: (-len(x[1]), x[0]))       # returns tuples of sorted items
+
+    for k, v in output:
+        result.append(f'{k}:')
+
+        for value in sorted(v):
+            result.append(f'-{value}')
+
+    print(result)
+
+    return '\n'.join(result)
+
+
+example_objects = {"Water Lilly": "flower",
+                   "Swifts": "bird",
+                   "Callery Pear": "tree",
+                   "Swallows": "bird",
+                   "Dahlia": "flower",
+                   "Tulip": "flower", }
+print(start_spring(**example_objects))
+
