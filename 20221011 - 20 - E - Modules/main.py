4ee0306f1a428a07894c3ca2e84ebc7df3b122aa
theterminal <kirilk777@gmail.com>
2022-10-26 08:34:39 +0300

    Initial

---
+++ 20221011 - 20 - E - Modules/main.py
@@ -0,0 +1,8 @@
+# link to Diyan's project: https://github.com/DiyanKalaydzhiev23/GUI-Shop
+from authentication import render_entry
+from canvas import root
+
+
+if __name__ == '__main__':
+    render_entry()
+    root.mainloop()
