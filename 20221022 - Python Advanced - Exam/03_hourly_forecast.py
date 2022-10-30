2e1bb1f9c9f994c6e923d4a7af7c777c10b96ab2
theterminal <kirilk777@gmail.com>
2022-10-30 07:15:52 +0200

    Initial

---
+++ 20221022 - Python Advanced - Exam/03_hourly_forecast.py
@@ -0,0 +1,35 @@
+# 20221022 - Python Advanced - Exam
+# 03 - Hourly Forecast - judge: https://judge.softuni.org/Contests/Compete/Index/3596#2
+
+
+def forecast(*args):
+    weather = {'Sunny': [], 'Cloudy': [], 'Rainy': []}
+
+    for k, v in args:
+        weather[v] += [k]
+
+    result = ''
+    for condition, locations in weather.items():
+        for location in sorted(locations):
+            result += f'{location} - {condition}\n'
+
+    return result
+
+
+print(forecast(
+    ("Sofia", "Sunny"),
+    ("London", "Cloudy"),
+    ("New York", "Sunny")))
+
+print(forecast(
+    ("Beijing", "Sunny"),
+    ("Hong Kong", "Rainy"),
+    ("Tokyo", "Sunny"),
+    ("Sofia", "Cloudy"),
+    ("Peru", "Sunny"),
+    ("Florence", "Cloudy"),
+    ("Bourgas", "Sunny")))
+
+print(forecast(
+    ("Tokyo", "Rainy"),
+    ("Sofia", "Rainy")))
