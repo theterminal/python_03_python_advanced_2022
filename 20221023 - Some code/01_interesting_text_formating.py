bf043a783cbea359e9d9e0a5c9276722da3b7b01
theterminal <kirilk777@gmail.com>
2022-10-31 06:44:51 +0200

    Initial

---
+++ 20221023 - Some code/01_interesting_text_formating.py
@@ -0,0 +1,18 @@
+text = 'EPIC'
+
+print('\n----------- ex. 1 --------------\n')
+
+print(text)
+print(f'{text}')
+
+print('\n----------- ex. 2 --------------\n')
+
+print(f'{text:.<20}')
+print(f'{text:.>20}')
+print(f'{text:.^20}')
+
+print('\n----------- ex. 3 --------------\n')
+
+print(f'{text:*<30}')
+print(f'{text:_>30}')
+print(f'{text:.^30}')
